2697588d8c1fdc4066d8846f1c77cbc169a05ec7
chjung <heeyame@gmail.com>
2019-02-26 23:12:31 +0900

    7.4장 자바스크립트 웹토큰과 OAuth2 소스 커밋

--- spmia-chapter7-JWT/authentication-service/src/main/java/com/thoughtmechanix/authentication/security/JWTOAuth2Config.java
+++ spmia-chapter7-JWT/authentication-service/src/main/java/com/thoughtmechanix/authentication/security/JWTOAuth2Config.java
@@ -1,32 +1,22 @@
 package com.thoughtmechanix.authentication.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.Store;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-
 import java.util.Arrays;
 
 @Configuration
-public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -34,19 +24,41 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private DefaultTokenServices tokenServices;
+
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Autowired
+    private TokenEnhancer jwtTokenEnhancer;
+
+
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtTokenEnhancer, jwtAccessTokenConverter));
+
+        endpoints.tokenStore(tokenStore)                             //JWT
+                .accessTokenConverter(jwtAccessTokenConverter)       //JWT
+                .tokenEnhancer(tokenEnhancerChain)                   //JWT
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+
+
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
         clients.inMemory()
                 .withClient("eagleeye")
                 .secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("thisissecret"))
                 .authorizedGrantTypes("refresh_token", "password", "client_credentials")
                 .scopes("webclient", "mobileclient");
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-      endpoints
-        .authenticationManager(authenticationManager)
-        .userDetailsService(userDetailsService);
     }
 }
